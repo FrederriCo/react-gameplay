e512a501a516a2897a5464ad6d0193e133d6ed41
Ferdi Mehmedov <FredericCo@mail.bg>
2023-04-24 14:35:28 +0300

    Create LatsetGame

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,72 +1,22 @@
-const Home = () => {
+import LatestGame from "./LatestGame";
+
+const Home = ({ games }) => { 
     return (
         <section id="welcome-world">
-                <div className="welcome-message">
-                    <h2>ALL new games are</h2>
-                    <h3>Only in GamesPlay</h3>
-                </div>
-                <img src="./images/four_slider_img01.png" alt="hero" />
-                <div id="home-page">
-                    <h1>Latest Games</h1>
-                    {/* Display div: with information about every game (if any) */}
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/CoverFire.png" />
-                        </div>
-                        <h3>Cover Fire</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/ZombieLang.png" />
-                        </div>
-                        <h3>Zombie Lang</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/MineCraft.png" />
-                        </div>
-                        <h3>MineCraft</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                    {/* Display paragraph: If there is no games  */}
-                    <p className="no-articles">No games yet</p>
-                </div>
-            </section>
+            <div className="welcome-message">
+                <h2>ALL new games are</h2>
+                <h3>Only in GamesPlay</h3>
+            </div>
+            <img src="./images/four_slider_img01.png" alt="hero" />
+            <div id="home-page">
+                <h1>Latest Games</h1>
+
+                {games.length > 0
+                    ? games.map(x => <LatestGame key={x._id} game={x} />)
+                    : <p className="no-articles">No games yet</p>
+                }
+            </div>
+        </section>
     );
 };
 
