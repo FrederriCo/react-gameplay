657c363d05cfc1816edac48cbcd663228e06dd12
Ferdi Mehmedov <FredericCo@mail.bg>
2023-04-27 16:29:24 +0300

    Create AuthContext

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -3,7 +3,7 @@ import CatalogItem from './CatalogItem';
 const Catalog = ({ games }) => {
     return (
         <section id="catalog-page">
-            <h1>All Games</h1>           
+            <h1>All Games</h1>            
             {games.length > 0
                 ? games.map(x => <CatalogItem key={x._id} game={x} />)
                 :< h3 className="no-articles">No articles yet</h3>
