1b3d08a70bc40537aa32f58e0a7eb07e9ad791a8
Ferdi Mehmedov <FredericCo@mail.bg>
2023-04-28 10:21:47 +0300

    create logout service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,23 @@
 import * as request from "./utl/requester";
 
-const url = 'http://localhost:3031';
+const url = 'http://localhost:3031/users';
 
 export const login = (email, password) =>
-    request.post(`${url}/users/login`, { email, password });
+    request.post(`${url}/login`, { email, password });
+
+export const logout = async (accessToken) => {
+    try {
+        const response = await fetch(`${url}/logout`, {
+            headers: {
+                'X-Authorization': accessToken
+            }
+        });
+        return response;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const register = (email, password) =>
+    request.post(`${url}/register`, { email, password });
