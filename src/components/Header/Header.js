1745cd3462f8225f68a43f40b457f68bdd9dfecf
Ferdi Mehmedov <FredericCo@mail.bg>
2023-04-28 10:20:11 +0300

    In header components add email for login user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 const Header = () => {
-    return(
-        <header>
-        {/* Navigation */}
-        <h1>
-          <Link className="home" to="/">
-            GamesPlay
-          </Link>
-        </h1>
-        <nav>
-          <Link to="/catalog">All games</Link>
-          {/* Logged-in users */}
+  const { user } = useContext(AuthContext);
+  return (
+    <header>
+      {/* Navigation */}
+      <h1>
+        <Link className="home" to="/">
+          GamesPlay
+        </Link>
+      </h1>
+      <nav>
+        {user.email && <span>{user.email}</span>}
+        <Link to="/catalog">All games</Link>
+        {/* Logged-in users */}
+        {user.email
+          ?
           <div id="user">
             <Link to="/create">Create Game</Link>
             <Link to="/logout">Logout</Link>
           </div>
-          {/* Guest users */}
+          :
           <div id="guest">
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
           </div>
-        </nav>
-      </header>
-    );
+        }
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
