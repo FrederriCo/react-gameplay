85120cf9228bb42759f0f4dec770df71a590edb8
Ferdi Mehmedov <FredericCo@mail.bg>
2023-05-04 13:10:38 +0300

    Create custum hook and hok

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export const AuthContext = createContext();
@@ -15,10 +15,32 @@ export const AuthProvider = ({ children }) => {
     };
 
     return (
-        <AuthContext.Provider value={{ user: auth, userLogin, userLogout }} >
+        <AuthContext.Provider value={{
+            user: auth,
+            userLogin,
+            userLogout,
+            isAuthenticated: !!auth.accessToken
+        }} >
             {children}
         </AuthContext.Provider>
     );
 
 }
 
+// Custom Hook
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+
+    return context;
+};
+
+// With HOC
+export const withAuth = (Component) => {
+    const AuthWrapper = (props) => {
+        const context = useContext(AuthContext);
+
+        return <Component {...props} auth={context} />
+    }
+
+    return AuthWrapper;
+} 
