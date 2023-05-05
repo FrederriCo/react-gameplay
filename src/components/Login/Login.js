c7a9e8f93c0c7e0e959908459b1650e87496bff0
Ferdi Mehmedov <FredericCo@mail.bg>
2023-05-05 14:28:49 +0300

    Import useContext,  AuthContext and implement

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../services/authService";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
+    const { userLogin } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const onSubmit = (e) => {
@@ -11,37 +14,38 @@ const Login = () => {
 
         login(email, password)
             .then(authData => {
-                console.log(authData)
+                userLogin(authData);
+                navigate('/');
             })
             .catch(() => {
                 navigate('/404');
             });
 
-    }
+    };
     return (
         <section id="login-page" className="auth">
-        <form id="login" onSubmit={onSubmit}>
-            <div className="container">
-                <div className="brand-logo" />
-                <h1>Login</h1>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Sokka@gmail.com"
-                />
-                <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" />
-                <input type="submit" className="btn submit" value="Login" />
-                <p className="field">
-                    <span>
-                        If you don't have profile click <a href="#">here</a>
-                    </span>
-                </p>
-            </div>
-        </form>
-    </section>
+            <form id="login" onSubmit={onSubmit}>
+                <div className="container">
+                    <div className="brand-logo" />
+                    <h1>Login</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Sokka@gmail.com"
+                    />
+                    <label htmlFor="login-pass">Password:</label>
+                    <input type="password" id="login-password" name="password" />
+                    <input type="submit" className="btn submit" value="Login" />
+                    <p className="field">
+                        <span>
+                            If you don't have profile click <a href="#">here</a>
+                        </span>
+                    </p>
+                </div>
+            </form>
+        </section>
     );
 };
 
