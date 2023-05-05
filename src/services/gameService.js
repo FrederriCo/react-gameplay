de2140319d34ffe31be048f8ed9b6efb4d043411
Ferdi Mehmedov <FredericCo@mail.bg>
2023-05-05 14:25:18 +0300

    Create a few function

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,6 +1,13 @@
-const url = 'http://localhost:3031';
+import * as request from "./utl/requester";
 
-export const getAll = () => {
-   return fetch(`${url}/data/games`)
-    .then(res => res.json())    
-};
+const url = 'http://localhost:3031/data/games';
+
+export const getAll = () => request.get(url);
+
+export const getOneGame = (gameId) => request.get(`${url}/${gameId}`);
+
+export const create = (gameData) => request.post(url, gameData);
+
+export const edit = (gameId, gameData) => request.put(`${url}/${gameId}`, gameData);
+
+export const remove = (gameId) => request.del(`${url}/${gameId}`);
