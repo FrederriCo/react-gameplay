714e9ee529563523b7f296cf876a1a7f892fd8ee
Ferdi Mehmedov <FredericCo@mail.bg>
2023-05-10 10:52:00 +0300

    Refactoring App.js

--- src/App.js
+++ src/App.js
@@ -1,84 +1,68 @@
-import { useEffect, useState } from "react";
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import uniqid from 'uniqid';
+import { lazy, Suspense } from "react";
+import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import Logout from './components/Logout/Logout';
+//import Register from './components/Register/Register';
 import Create from './components/Create/Create';
 import Edit from './components/Edit/Edit';
 import Details from './components/Details/Details';
 import Catalog from './components/Catalog/Catalog';
-import * as gameService from './services/gameService';
+import { AuthProvider } from './context/AuthContext';
+import { GameProvider } from './context/GameContext';
+import PrivateRoute from './services/utl/PrivateRoute';
+import PrivateGuard from "./services/utl/PrivateGuard";
+import GameOwner from "./services/utl/GameOwner";
 
+
+const Register = lazy(() => import('./components/Register/Register'));
 
 function App() {
-    const [games, setGames] = useState([]);
-    const navigate = useNavigate();
-
-    const addComment = (gameId, comment) => {
-        setGames(state => {
-            const game = state.find(x => x._id == gameId);
-            console.log(game);
-            const comments = game.comments || [];
-            comments.push(comment);
-
-            return [
-                ...state.filter(x => x._id !== gameId),
-                {...game, comments}
-            ]
-        });
-    };
-
-    const addGameHandler = (gameData) => {
-        setGames(state => [
-            ...state,
-            {
-                ...gameData,
-                _id: uniqid(),
-            },
-        ]);
-
-        navigate('/catalog');
-    };
-
-    useEffect(() => {
-        gameService.getAll()
-            .then(result => {
-                setGames(result);
-            })
-    }, []);
 
     return (
-        <div id="box">
-            <Header />
+        <AuthProvider>
+            <div id="box">
+                <Header />
 
-            {/* Main Content */}
-            <main id="main-content">
+                {/* Main Content */}
+                <GameProvider>
+                    <main id="main-content">
+                        <Routes>
+                            {/*Home Page*/}
+                            <Route path="/" element={<Home />} />
+                            {/* Login Page ( Only for Guest users ) */}
+                            <Route path="/login" element={<Login />} />
+                            {/* Register Page ( Only for Guest users ) */}
+                            <Route path="/register" element={
+                                <Suspense fallback={<span>Loading....</span>}>
+                                    <Register />
+                                </Suspense>
+                            } />
+                            {/* Create Page ( Only for logged-in users ) */}
+                            <Route path="/create" element={(
+                                <PrivateRoute><Create /></PrivateRoute>
+                            )} />
+                            {/* Edit Page ( Only for the creator )*/}
+                            {/* Catalogue */}
+                            <Route path="/catalog" element={<Catalog />} />
+                            {/* Logout */}
+                            {/*Details Page*/}
+                            <Route element={<GameOwner />}>
+                                <Route path="games/:gameId/edit" element={<Edit />} />
+                            </Route>
+                            <Route path="/catalog/:gameId" element={<Details />} />
+                            <Route element={<PrivateGuard />}>
+                                <Route path="/logout" element={<Logout />} />
+                            </Route>
+                        </Routes>
+                    </main>
+                </GameProvider>
 
-                <Routes>
-                    {/*Home Page*/}
-                    <Route path="/" element={<Home games={games} />} />
-                    {/* Login Page ( Only for Guest users ) */}
-                    <Route path="/login" element={<Login />} />
-                    {/* Register Page ( Only for Guest users ) */}
-                    <Route path="/register" element={<Register />} />
-                    {/* Create Page ( Only for logged-in users ) */}
-                    <Route path="/create" element={<Create addGameHandler={addGameHandler} />} />
-                    {/* Catalogue */}
-                    <Route path="/catalog" element={<Catalog games={games} />} />
-                     {/*Details Page*/}
-                     <Route path="/catalog/:gameId" element={<Details games={games} addComment={addComment} />} />
-                </Routes>
-            </main>
-
-            {/* Edit Page ( Only for the creator )*/}
-            {/* <Edit /> */}           
-
-        </div>
-
+            </div>
+        </AuthProvider>
     );
 }
 
